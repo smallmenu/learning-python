bf34e9e47dea55e6d7740bb311cd50a532417f9d
smallmenu <smallmenu@163.com>
2023-08-08 16:10:22 +0800

    * init

--- guess.py
+++ guess.py
@@ -1,20 +1,25 @@
 import random
 
 pool = []
+flag = []
 for i in range(4):
-    pool.append(random.randint(1, 10))
+    if i > 0:
+        while True:
+            x = random.randint(1, 10)
+            if x not in pool:
+                pool.append(x)
+                break
+    else:
+        pool.append(random.randint(1, 10))
+
 print(pool)
 
-has = 0
-success = 0
-
 while True:
+    has = 0
+    success = 0
+
     # 字符串
     guess = input('Enter number with space:')
-
-    # 退出
-    if guess == 'q':
-        break
 
     # 空格分割
     inputs = guess.split(' ')
@@ -27,10 +32,15 @@ while True:
     print(inputs)
 
     # 比较
-    for i in range(len(inputs)):
+    for i in range(len(pool)):
         if inputs[i] == pool[i]:
             success += 1
         elif inputs[i] in pool:
             has += 1
 
+    if success == 4:
+        break
+
     print('has:{0}, success:{1}'.format(has, success))
+
+print('Correct')
